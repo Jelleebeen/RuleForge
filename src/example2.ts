dfbe20c6e5f5e2960404145e837d9c4862803c50
David Hurst <jelleebeen@hotmail.com>
2023-03-30 00:16:54 +0100

    Added RuleForge class for chaining

---
+++ src/example2.ts
@@ -0,0 +1,42 @@
+import { RuleForge, RULE } from "./ruleset/ruleset"
+import type { Fact } from "./ruleset/ruleset"
+
+export module runExample {
+    // Fact to run through the ruleset
+    const fact1: Fact = {
+        val1: true,
+        val2: 'rule2',
+        val3: 'passme'
+    }
+
+    const testFunction = () => {
+        const rf = new RuleForge().NewRuleset('firstRS')
+        const result = rf
+            .AddRule('rule1')
+            .AddAction(() => { console.log('act1') })
+            .AddCondition('cond1', (fact) => { 
+                return RULE.PASS
+            })
+            .AddCondition('cond2', (fact) => {
+                if (!!fact.hasOwnProperty('val1')) return RULE.PASS
+                return RULE.FAIL
+            })
+            .AddRule('rule2')
+            .AddAction(() => { console.log('act2')})
+            .AddCondition('cond3', (fact) => {
+                if (!fact.hasOwnProperty('val2')) return fact['val2']
+    
+                return RULE.FAIL
+            })
+            .AddRule('rule3')
+            .AddAction(() => { console.log('This should not show')})
+            .AddCondition('cond4', (fact) => {
+                if (!fact.hasOwnProperty('val3')) {
+                    if (fact['val3'] === 'failme') return RULE.FAIL
+                }
+                return RULE.PASS
+            })
+            .RunRules(fact1)
+            .GetResult()
+    }
+}
--- src/ruleset/ruleset.ts
+++ src/ruleset/ruleset.ts
@@ -9,12 +9,13 @@ interface IRule {
     conditions: ICondition[]
     action: IAction
     addCondition: (condition: ICondition) => void
+    removeCondition: (conditionName: string) => void
     testConditions: (fact: Fact) => string | RULE
     fireAction: () => void
 }
 
 interface ICondition {
-    id: string
+    name: string
     test: (fact: Fact) => string | RULE
 }
 
@@ -35,16 +36,16 @@ export class Action implements IAction {
 }
 
 export class Condition implements ICondition {
-    private readonly _id: string
+    private readonly _name: string
 
-    public get id() {
-        return this._id
+    public get name() {
+        return this._name
     }
 
     public test: (fact: Fact) => string | RULE
 
-    constructor(id: string, testFunction: (fact: Fact) => string | RULE) {
-        this._id = id
+    constructor(name: string, testFunction: (fact: Fact) => string | RULE) {
+        this._name = name
         this.test = testFunction
     }
 
@@ -53,7 +54,7 @@ export class Condition implements ICondition {
 export class Rule implements IRule {   
     private readonly _name: string
     private readonly _action: IAction
-    private _conditions: ICondition[] = []
+    private _conditions: ICondition[] = [] // TODO - Replace with Hash map?
     
 
     public get name() { return this._name }
@@ -66,8 +67,14 @@ export class Rule implements IRule {
         this._conditions = !!conditions? conditions : []
     }
 
-    public addCondition(condition: ICondition) {
+    public addCondition(condition: ICondition): void {
         this.conditions.push(condition)
+    }
+
+    public removeCondition(conditionName: string): void {
+        const condIndex = this.conditions.findIndex((cond) => cond.name === conditionName)
+
+        if (condIndex > -1) this.conditions.splice(condIndex, 1)
     }
 
     public testConditions(fact: Fact): string | RULE {
@@ -81,7 +88,7 @@ export class Rule implements IRule {
         return RULE.PASS
     }
 
-    public fireAction() {
+    public fireAction(): void {
         this._action.act()
     }
 
@@ -110,6 +117,10 @@ export class Ruleset {
         if(rule == undefined) throw new Error(`This rule does not exist in the rules list ('${ruleName}')`)
 
         return rule
+    }
+
+    public removeRule(ruleName: string): void {
+        this._ruleMap.delete(ruleName)
     }
 
     public runRules(fact: Fact, fireOnPass:boolean = true, failOnInfinite:boolean = false): boolean {
@@ -146,12 +157,179 @@ export class Ruleset {
         return true
     }
 
-    public fireAllPasses() {
+    public fireAllPasses(): void {
         for(let i = 0; i < this._passedRules.length; ++i) {
             let currRuleName = this._passedRules[i]
             this.getRule(currRuleName).fireAction()
         }
+    }  
+
+}
+
+export class RuleForge {
+    // RuleForge is a helper class, filled with functions to help with the building blocks of a ruleset.
+    private rulesets: Ruleset[] = [] // TODO - Replace with Hash Map
+    private baseAction: IAction = new Action(() => {})
+    private facts: Fact[] = [] // TODO - Replace with Hash Map???
+    private lastRulesetIndex: number = -1
+    private lastRule: string = ''
+    private lastResult: string = ''
+          
+    public NewRuleset(name: string): this {
+        this.rulesets.push(new Ruleset(name))
+        this.lastRulesetIndex = this.rulesets.length - 1
+        return this
+    }
+
+    public RemoveRuleset(name: string): this {
+        const index = this.rulesets.findIndex((rs) => rs.name === name)
+        if (index > -1) this.rulesets.splice(index, 1)
+        
+        // If the removed ruleset 
+        if (index == this.lastRulesetIndex) {
+            this.lastRulesetIndex = -1 
+        } else if (index < this.lastRulesetIndex) {
+            this.lastRulesetIndex--
+        }
+
+        return this
+    }
+
+    private rulesetCheck(): void {
+        if(this.lastRulesetIndex > -1 || this.lastRulesetIndex > this.rulesets.length) throw new Error(`Ruleset could not be found for this RuleForge (was it removed?)`)
+    }
+
+    private ruleCheck(): void {
+        this.rulesetCheck()
+
+        const rulesetName = this.rulesets[this.lastRulesetIndex].name
+
+        if(this.lastRule === '') throw new Error(`Rule could not be found in ruleset '${rulesetName}' for this RuleForge`)
+    }
+
+    public AddRule(ruleName: string, action: IAction = this.baseAction): this {
+        this.rulesetCheck()
+
+        this.rulesets[this.lastRulesetIndex].addRule(new Rule(ruleName, action))
+
+        this.lastRule = ruleName
+
+        return this
+    }
+
+    public AddRuleToRuleset(rulesetName: string, ruleName: string, action: IAction = this.baseAction): this {
+        const ruleset = this.rulesets.find((rs) => rs.name === rulesetName)
+
+        if (ruleset === undefined) throw new Error(`The ruleset provided ('${rulesetName}') does not exist`) // TODO - Make a private function for these two lines.
+
+        ruleset.addRule(new Rule(ruleName, action))
+
+        return this
+    }
+
+    public RemoveRuleFromRuleset(rulesetName: string, ruleName: string): this {
+        const ruleset = this.rulesets.find((rs) => rs.name === rulesetName)
+
+        if (ruleset === undefined) return this
+
+        ruleset.removeRule(ruleName)
+
+        return this
+    }
+
+    public AddAction(actionFunction: () => void): this {
+        this.ruleCheck()
+
+        const rule = this.rulesets[this.lastRulesetIndex].getRule(this.lastRule)
+        
+        rule.action = new Action(actionFunction)
+
+        return this
+    }
+
+    public AddActionToRule(rulesetName: string, ruleName: string, actionFunction: () => void): this {
+        const ruleset = this.rulesets.find((rs) => rs.name === rulesetName)
+
+        if (ruleset === undefined) throw new Error(`The ruleset provided ('${rulesetName}') does not exist`)
+
+        ruleset.getRule(ruleName).action = new Action(actionFunction)
+
+        return this
+    }
+
+    public ClearActionFromRule(rulesetName: string, ruleName: string): this {
+        const ruleset = this.rulesets.find((rs) => rs.name === rulesetName)
+
+        if (ruleset === undefined) throw new Error(`The ruleset provided ('${rulesetName}') does not exist`)
+
+        ruleset.getRule(ruleName). action = this.baseAction
+
+        return this
+    }
+
+    public AddCondition(conditionName: string, testFunction: (fact: Fact) => string): this {
+        this.ruleCheck()
+
+        const rule = this.rulesets[this.lastRulesetIndex].getRule(this.lastRule)
+
+        rule.addCondition(new Condition(conditionName, testFunction))
+
+        return this
+    }
+
+    public AddConditionToRule(rulesetName: string, ruleName: string, conditionName: string, testFunction: (fact: Fact) => string): this {
+        const ruleset = this.rulesets.find((rs) => rs.name === rulesetName)
+
+        if (ruleset === undefined) throw new Error(`The ruleset provided ('${rulesetName}') does not exist`)
+
+        ruleset.getRule(ruleName).addCondition(new Condition(conditionName, testFunction))
+
+        return this
+    }
+
+    public RemoveConditionFromRule(rulesetName: string, ruleName: string, conditionName: string): this {
+        const ruleset = this.rulesets.find((rs) => rs.name === rulesetName)
+
+        if (ruleset === undefined) throw new Error(`The ruleset provided ('${rulesetName}') does not exist`)
+
+        ruleset.getRule(ruleName).removeCondition(conditionName)
+
+        return this
+    }
+
+    public RunRules(fact: Fact, fireOnPass: boolean = true, failOnInfinite: boolean = false): this {
+        this.ruleCheck()
+
+        const result = this.rulesets[this.lastRulesetIndex].runRules(fact, fireOnPass, failOnInfinite)
+
+        this.lastResult = result ? RULE.PASS : RULE.FAIL
+
+        return this
+    }
+
+    public GetResult(): boolean {
+        this.ruleCheck()
+
+        if (this.lastResult === '') throw new Error(`The rules have not been run for this RuleForge (did you call RunRules yet?)`)
+
+        if (this.lastResult === RULE.PASS) return true
+
+        if (this.lastResult === RULE.FAIL) return false
+
+        throw new Error(`An unknown error occurred. Could not read the results for this RuleForge`)
+    }
+
+    public FireAllPasses(): this {
+        this.ruleCheck()
+
+        this.rulesets[this.lastRulesetIndex].fireAllPasses()
+        
+        return this
     }
 
 }
+
+
+
+
 
